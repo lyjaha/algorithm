c8455494a0911da3e06d346b155f2e4034a20eaa
lishuanglong <394532736@qq.com>
2023-07-26 23:53:34 +0800

    链表中的节点每 k 个一组翻转

--- src/main/java/com/ly/nk/list/ReverseBetween.java
+++ src/main/java/com/ly/nk/list/ReverseBetween.java
@@ -3,6 +3,7 @@ package com.ly.nk.list;
 /**
  * 2.链表内指定区间反转
  *
+ * 描述：
  * 将一个节点数为 size 链表 m 位置到 n 位置之间的区间反转，要求时间复杂度 O(n)O(n)，空间复杂度 O(1)O(1)。
  * 例如：
  * 给出的链表为 1 → 2 → 3 → 4 → 5 → NULL, m=2,n=4,
@@ -42,6 +43,9 @@ public class ReverseBetween {
      * step 4：对于从m到n这些个位置的节点，依次断掉指向后续的指针，反转指针方向。
      * step 5：返回时去掉添加的表头。
      *
+     * 时间复杂度：O(n)，最坏情况下需要遍历全部链表节点，比如m为链表最后一个位置，或者n为链表最后一个位置时
+     * 空间复杂度：O(1)，常数级指针变量，无额外辅助空间使用
+     *
      * @param head ListNode类
      * @param m int整型
      * @param n int整型
@@ -73,5 +77,44 @@ public class ReverseBetween {
         }
         //返回去掉表头
         return res.next;
+    }
+
+    /**
+     * 方法2：递归
+     *
+     * step 1：准备全局变量temp，最初等于null，找到递归到第n个节点时，指向其后一个位置，要将反转部分的起点
+     * （即反转后的尾）连接到这个指针。
+     * step 2：按照第一个递归的思路缩短子问题找到反转区间的起点，将反转后的部分拼接到前面正常的后面。
+     * step 3：按照第二个递归的思路缩短终点的子问题，从第n个位置开始反转，反转过程中每个子问题作为反转后的尾，都要指向temp。
+     *
+     * 时间复杂度：O(n)，最坏情况下递归遍历全部链表节点，比如m为链表最后一个位置，或者n为链表最后一个位置时
+     * 空间复杂度：O(n)，遍历全部节点时递归栈深度最坏为n
+     *
+     */
+
+    ListNode temp = null;
+    public ListNode reverse(ListNode head, int n){
+        //只颠倒第一个节点，后续不管
+        if(n == 1){
+            temp = head.next;
+            return head;
+        }
+        //进入子问题
+        ListNode node = reverse(head.next, n - 1);
+        //反转
+        head.next.next = head;
+        //每个子问题反转后的尾拼接第n个位置后的节点
+        head.next = temp;
+        return node;
+    }
+    public ListNode reverseBetween2 (ListNode head, int m, int n) {
+        //从第一个节点开始
+        if(m == 1)
+            return reverse(head, n);
+        //缩减子问题
+        ListNode node = reverseBetween2(head.next, m - 1, n - 1);
+        //拼接已翻转
+        head.next = node;
+        return head;
     }
 }
---
+++ src/main/java/com/ly/nk/list/ReverseKGroup.java
@@ -0,0 +1,75 @@
+package com.ly.nk.list;
+
+/**
+ * 3.链表中的节点每 k 个一组翻转
+ *
+ * 描述：
+ * 将给出的链表中的节点每 k 个一组翻转，返回翻转后的链表
+ * 如果链表中的节点数不是 k 的倍数，将最后剩下的节点保持原样
+ * 你不能更改节点中的值，只能更改节点本身。
+ *
+ * 数据范围：0 ≤ n ≤ 2000 ，1 ≤ k ≤ 2000 ，链表中每个元素都满足 0 ≤ val ≤ 1000
+ * 要求 空间复杂度 O(1)，时间复杂度 O(n)
+ *
+ * 例如：
+ * 给定的链表是 1 → 2 → 3 → 4 → 5
+ * 对于 k = 2 , 你应该返回 2 → 1 → 4 → 3 → 5
+ * 对于 k = 3 , 你应该返回 3 → 2 → 1 → 4 → 5
+ *
+ * 举一反三：
+ * 学习完本题的思路你可以解决如下题目：
+ *
+ * BM1.反转链表
+ * BM2.链表内指定区间反转
+ */
+public class ReverseKGroup {
+
+     public class ListNode {
+       int val;
+       ListNode next = null;
+       public ListNode(int val) {
+         this.val = val;
+       }
+     }
+
+
+    /**
+     * 步骤：
+     * step 1：每次从进入函数的头节点优先遍历链表k次，分出一组，若是后续不足k个节点，不用反转直接返回头。
+     * step 2：从进入函数的头节点开始，依次反转接下来的一组链表，反转过程同BM1.反转链表。
+     * step 3：这一组经过反转后，原来的头变成了尾，后面接下一组的反转结果，下一组采用上述递归继续。
+     *
+     *
+     * 时间复杂度：O(n)，一共遍历链表 n 个节点
+     * 空间复杂度：O(n)，递归栈最大深度 为n/k
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode reverseKGroup (ListNode head, int k) {
+        //找到每次翻转的尾部
+        ListNode tail = head;
+        //遍历k次到尾部
+        for(int i = 0; i < k; i++){
+            //如果不足k到了链表尾，直接返回，不翻转
+            if(tail == null)
+                return head;
+            tail = tail.next;
+        }
+        //翻转时需要的前序和当前节点
+        ListNode pre = null;
+        ListNode cur = head;
+        //在到达当前段尾节点前
+        while(cur != tail){
+            //翻转
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        //当前尾指向下一段要翻转的链表
+        head.next = reverseKGroup(tail, k);
+        return pre;
+    }
+}
