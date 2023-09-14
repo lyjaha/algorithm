b954b2337563158b2e6da93aa042b326a5b3ef7a
lishuanglong <394532736@qq.com>
2023-09-14 23:45:18 +0800

    BM1.反转链表

--- src/main/java/com/ly/nk/list/ReverseList.java
+++ src/main/java/com/ly/nk/list/ReverseList.java
@@ -19,7 +19,7 @@ package com.ly.nk.list;
  */
 public class ReverseList {
 
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next = null;
         public ListNode(int val) {
@@ -28,41 +28,43 @@ public class ReverseList {
     }
 
     /**
-     *  方式1：迭代
+     *  方式1：双指针迭代
      *
      * 思路：
      * 将链表反转，就是将每个表元的指针从向后变成向前，那我们可以遍历原始链表，将遇到的节点一一指针逆向即可。
      * 指针怎么逆向？不过就是断掉当前节点向后的指针，改为向前罢了。
      *
      * 步骤：
-     * 1：优先处理空链表，空链表不需要反转。
-     * 2：我们可以设置两个指针，一个当前节点的指针，一个上一个节点的指针（初始为空）。
-     * 3：遍历整个链表，每到一个节点，断开当前节点与后面节点的指针，并用临时变量记录后一个节点，然后当前节点指向上一个节点，即可以将指针逆向。
+     * 1：先处理空链表，空链表不需要反转。
+     * 2：设置两个指针，一个当前节点的指针，一个上一个节点的指针（初始为空）。
+     * 3：遍历整个链表，每到一个节点，断开当前节点与后面节点的指针，并用临时变量记录后一个节点，
+     * 然后当前节点指向上一个节点，即可以将指针逆向。
      * 4：再轮换当前指针与上一个指针，让它们进入下一个节点及下一个节点的前序节点。
      *
      *
      * 时空复杂度：
-     *  时间复杂度：O(n),一次循环，只遍历链表一次
-     *  空间复杂度：O(1),常数级变量，无需额外辅助空间使用
+     *  时间复杂度：O(n),其中n为链表的长度。需要遍历整个链表才能完成反转操作。
+     *  空间复杂度：O(1),只需要常量级别的额外空间用于指针操作。
      *
      * @param head
      * @return
      */
-    public ListNode ReverseList (ListNode head) {
+    public static ListNode reverseList (ListNode head) {
         // 处理空链表
         if (null == head) {
             return null;
         }
         ListNode cur = head;
         ListNode pre = null;
+
         while (null != cur) {
-            // 断开链表，记录后续1个
+            // 先保存访问的节点的下一个节点
             ListNode temp = cur.next;
-            // 当前next指向前1个
+            // 反转指针
             cur.next = pre;
-            // 前1个更新为当前
+            // pre指针向后移动
             pre = cur;
-            // 当前更新为刚刚记录的最后1个
+            // cur指针向后移动
             cur = temp;
         }
         return pre;
@@ -85,16 +87,42 @@ public class ReverseList {
      * 时间复杂度：O(n)，相当于递归遍历一次链表
      * 空间复杂度：O(n)，递归栈深度为链表长度n
      */
-    public ListNode ReverseList2(ListNode head) {
+    public ListNode reverseList2(ListNode head) {
         //递归结束条件
         if(head == null || head.next == null)
             return head;
         //反转下一个
-        ListNode newHead = ReverseList2(head.next);
+        ListNode newHead = reverseList2(head.next);
         //逆转本级节点
         head.next.next = head;
         //尾部设置空节点
         head.next = null;
         return newHead;
     }
+
+    // 测试方法
+    public static void main(String[] args) {
+        // 创建测试链表 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(3);
+        ListNode node3 = new ListNode(4);
+        ListNode node4 = new ListNode(5);
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        // 反转链表
+        ListNode reversedHead = reverseList(head);
+
+        // 打印反转后的链表
+        ListNode node = reversedHead;
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+    }
+
+
 }
